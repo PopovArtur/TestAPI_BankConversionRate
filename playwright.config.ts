6789b7bf3da4dd429ea40015ebeb0f33db5e2a19
apopov <artur.popov_contractor@recordedfuture.com>
2023-10-16 15:28:59 -0400

    Add HTML reporting functionality

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from '@playwright/test';
 export default defineConfig({
+  globalTeardown:require.resolve('./global-teardown'),
   use: {
-    baseURL: 'https://www.bankofcanada.ca'
+    baseURL: 'https://www.bankofcanada.ca',
+    screenshot: 'only-on-failure'
   },
+  reporter: [ ['junit', { outputFile: 'results.xml' }] ]
 });
